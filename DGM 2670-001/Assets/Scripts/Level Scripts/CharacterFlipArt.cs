6f74c907569dc768a8fe9edf947b5368a6a05416
Taylor Gates <tayloranng@msn.com>
2016-10-03 13:59:20 -0600

    Flip Character script/scene

---
+++ DGM 2670-001/Assets/Scripts/Level Scripts/CharacterFlipArt.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterFlipArt : MonoBehaviour {
+
+    public Transform characterArt;
+
+    public bool forward = true;
+
+    void FlipCharacter (bool _b)
+    {
+        characterArt.Rotate(0, 180, 0);
+        forward = _b;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	if(Input.GetKeyDown(KeyCode.RightArrow) && forward)
+        {
+            FlipCharacter(false);
+        }
+        {
+    if (Input.GetKeyDown(KeyCode.LeftArrow) && !forward)
+        {
+                FlipCharacter(true);
+        }
+      }
+	}
+}
