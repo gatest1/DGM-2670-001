20ff9f57864382394c10d6f49de25280899cb704
Taylor Gates <tayloranng@msn.com>
2016-10-11 12:37:11 -0600

    10/9/16-Friday work

---
+++ DGM 2670-001/Assets/Scripts/Enemies/EnemyController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	public CharacterController myCC;
+	public float gravity = 1;
+	public Vector3 tempPOS;
+	
+	// Update is called once per frame
+	void Update () {
+		tempPOS.y -= gravity * Time.deltaTime;
+		myCC.Move (tempPOS);
+	}
+}
