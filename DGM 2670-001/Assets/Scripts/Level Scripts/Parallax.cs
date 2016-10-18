b9b8e3516a75014de57497b3a0fd7a198d165289
Taylor Gates <tayloranng@msn.com>
2016-10-18 15:39:49 -0600

    10/18/2016 - Sprint 3
    Got my parallaxing working, and the recycling is working.

---
+++ DGM 2670-001/Assets/Scripts/Level Scripts/Parallax.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour {
+
+
+	public float scrollSpeed = 0.03f;
+	public Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float offset = Time.time * scrollSpeed;
+		rend.material.mainTextureOffset = new Vector2 (offset, 0);
+	}
+}
