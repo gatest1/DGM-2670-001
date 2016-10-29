fa634ffd5075208d482ef78ee521c96fb1a31a2c
Taylor Gates <tayloranng@msn.com>
2016-10-29 13:44:19 -0600

    10/29/2016-Got my Timer and Game Over Screen working

---
+++ DGM 2670-001/Assets/Scripts/Level Scripts/Timer.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+	public Text timerText;
+	public float myTimer = 60;
+	private bool gameOver = false;
+	public Text gameOverText;
+	private bool timerIsActive = true;
+
+	void Start ()
+	{
+		timerText = GetComponent<Text> ();
+
+		gameOverText.text = "";
+	}
+
+	void Update ()
+	{
+	if (timerIsActive)
+	{
+		myTimer -= Time.deltaTime;
+		timerText.text = "Time Left: " + myTimer.ToString ("f0");
+		if (myTimer < 0)
+		{
+			myTimer = 0;
+			timerIsActive = false;
+			GameOver ();
+		}
+	}
+
+}
+
+	public void GameOver ()
+{
+	gameOverText.text = "Game Over!";
+	gameOver = true;
+
+	SceneManager.LoadScene (0);
+}
+
+
+
+	/*pulbic Text timerText;
+	private float startTime;
+
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float t = Time.time - startTime;
+
+		string minutes = ((int)t / 60).ToString ();
+		string seconds = (t % 60).ToString ("f2");
+
+		timerText.text = minutes + ":" + seconds;
+	}*/
+}
