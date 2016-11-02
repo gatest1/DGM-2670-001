76880862869ce153a8dc30fae356e2fda75ade73
Taylor Gates <tayloranng@msn.com>
2016-11-02 13:11:44 -0600

    11/2/2016 - Little fixes1

---
+++ DGM 2670-001/Assets/AddToScore.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AddToScore : MonoBehaviour {
+
+	public int count;
+	public Text Score;
+
+	// Use this for initialization
+	void Start () {
+		count = 0;
+		SetCountText ();
+	
+	}
+
+	void OnTriggerEnter()
+	{
+		count = count + 1;
+		SetCountText ();
+	}
+	
+	void SetCountText()
+	{
+		Score.text = "Score: " + count.ToString ();
+	}
+}
