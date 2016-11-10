7ab5ca785c64af11dc7fab80cbc27ccebd54e84f
Taylor Gates <tayloranng@msn.com>
2016-11-10 14:40:21 -0700

    11/10/2016 Wolf AI attempts1
    Worked with Brad to get my Wolf AI working.  Tumbleweed is acting weird.

---
+++ DGM 2670-001/Assets/Level Scripts/WolfJump.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WolfJump : MonoBehaviour {
+
+	public float speed = 1;
+	private Vector3 tempPos;
+	public float thrust = 15;
+	public Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		tempPos.x = speed * Time.deltaTime;
+		transform.Translate(tempPos);
+
+	}
+
+	void OnCollisionEnter (Collision collision)
+	{
+		if (collision.gameObject.tag == "Gap")
+			rb.AddForce (0, thrust, 0, ForceMode.Impulse);
+		print ("Cliff Jumper!");
+
+	}
+}
