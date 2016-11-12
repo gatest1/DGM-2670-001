bae02bc65c5b617b9e2dbcc70fd23e8e1c32b0c2
Taylor Gates <tayloranng@msn.com>
2016-11-12 13:57:47 -0700

    11/12/2016-Wolf going over gaps3

---
+++ DGM 2670-001/Assets/WolfJumpOriginal.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WolfJumpOriginal : MonoBehaviour {
+
+	public GameObject player;
+	public Vector3 tempPos;
+	public Transform wolf;
+
+	public bool canFlipWolf = true;
+
+	public float speed = 5;
+	public float gravity = 10;
+	public float jumpPower = 5;
+
+	void WolfFlipper ()
+	{
+		if (player.transform.position.x < wolf.transform.position.x && canFlipWolf) 
+		{
+			wolf.Rotate (0, -180, 0);
+			canFlipWolf = false;
+		}
+		if (player.transform.position.x > wolf.transform.position.x && !canFlipWolf) 
+		{
+			wolf.Rotate (0, 180, 0);
+			canFlipWolf = true;
+		}
+	}
+	IEnumerator WolfMover()
+	{
+		transform.position = Vector3.MoveTowards (transform.position, new Vector3 (player.transform.position.x, transform.position.y, transform.position.z), speed*Time.deltaTime);
+		yield return new WaitForEndOfFrame ();
+		StartCoroutine (WolfMover ());
+		WolfFlipper ();
+	}
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine (WolfMover ());
+	}
+	
+
+}
--- DGM 2670-001/Assets/WolfMovement.cs
+++ DGM 2670-001/Assets/WolfMovement.cs
@@ -1,15 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class WolfMovement : MonoBehaviour {
+public class WolfMovement : MonoBehaviour 
+{
 	public GameObject player;
 	public Vector3 tempPos;
 	public Transform wolf;
+	public CharacterController character;
 
 	public bool canFlipWolf = true;
 
 	public float speed = 5;
-	public float gravit = 10;
+	public float gravity = 10;
 	public float jumpPower = 5;
 
 	void WolfFlipper () {
@@ -17,26 +19,52 @@ public class WolfMovement : MonoBehaviour {
 		{
 			wolf.Rotate (0, -180, 0);
 			canFlipWolf = false;
+			tempPos.x = -speed;
 		}
 		if (player.transform.position.x > wolf.transform.position.x && !canFlipWolf) 
 		{
 			wolf.Rotate (0, 180, 0);
 			canFlipWolf = true;
+			tempPos.x = speed;
 		}
 	}
 
 	IEnumerator WolfMover()
 	{
-		transform.position = Vector3.MoveTowards (transform.position, new Vector3 (player.transform.position.x, transform.position.y, transform.position.z), speed * Time.deltaTime);
-		yield return new WaitForEndOfFrame ();
-		StartCoroutine (WolfMover ());
-		WolfFlipper ();
+		while (true) 
+		{
+			//if (player.transform.position.x > transform.position.x) {
+			//	tempPos.x = speed;
+			//} 
+			//else 
+			//{
+			//	tempPos.x = -speed;
+		//	}
+			tempPos.y -= gravity;
+			character.Move (tempPos * Time.deltaTime);
+			//transform.position = Vector3.MoveTowards (transform.position, new Vector3 (player.transform.position.x, transform.position.y, transform.position.z), speed * Time.deltaTime);
+			yield return new WaitForSeconds (.02f);
+			//StartCoroutine (WolfMover ());
+			WolfFlipper ();
+		}
 	}
+
+	//void OnTriggerEnter (Collider Gap)
+	//{
+	//	if (Gap.gameObject.tag == "Gap")
+	//	{tempPos.y = jumpPower;
+		//rigidbody.AddForce (jump, 0, 0, ForceMode.Impulse);
+	//		print ("Cliff Jumper!");}
+//	}
 
 	void Start ()
 	{
+		tempPos.x = speed;
 		StartCoroutine (WolfMover ());
 	}
 
-
+	public void jump ()
+	{
+		tempPos.y = jumpPower;
+	}
 }
