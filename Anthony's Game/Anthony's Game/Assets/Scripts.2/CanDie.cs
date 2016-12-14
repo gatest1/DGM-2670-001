0ac6a3b24765ec2da874d9974999f15147893fae
Taylor Gates <tayloranng@msn.com>
2016-12-14 13:23:51 -0700

    Last Min Changes

---
+++ Anthony's Game/Anthony's Game/Assets/Scripts.2/CanDie.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+	public interface ICanDie
+	{
+	void CanDie(); 
+	}
+
+	public interface IHurt
+	{
+		void Hurt ();
+	}
+
+	public interface ITakeDamage
+	{
+		void TakeDamage (); 
+	}
+
